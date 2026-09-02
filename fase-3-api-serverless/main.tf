3b3597131749131814ae5a944089d471eb3643a9
Joabe Diniz <joabediniz28@gmail.com>
2026-09-02 10:40:40 -0300

    Configure Terraform with AWS provider and S3 backend
    Added Terraform configuration for AWS provider and S3 backend.

--- fase-3-api-serverless/main.tf
+++ fase-3-api-serverless/main.tf
@@ -1,1 +1,22 @@
-# Configurações globais do Terraform.
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "logitrack-939603205666-us-east-2-an"
+    key     = "fase-3-serverless/terraform.tfstate" # Chave exclusiva para isolar esta fase
+    region  = "us-east-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
